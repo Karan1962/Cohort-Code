44d3b1a11bbf2f935270e8b9ae488238f65de85a
karan <karansawan5050@gmail.com>
2024-12-25 03:57:32 +0530

    react routing and lazy loading

--- Week-7/src/App.jsx
+++ Week-7/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// imported components through React.lazy
+const Dashboard = React.lazy(() => import("./components/Dashboard.jsx"));
+const Landing = React.lazy(() => import("./components/Landing.jsx"));
+const PageNotFound = React.lazy(() => import("./components/PageNotFound.jsx"));
 
+// also can import through normal way to test lazy loading in network tab
+// import Dashboard from "./components/Dashboard";
+// import Landing from "./components/Landing";
+// import PageNotFound from "./components/PageNotFound";
+
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* We should use Suspense whenever we want to implement React.lazy */}
+          <Route
+            path="/"
+            element={
+              //   <Suspense fallback={"loading...."}>
+              <Landing />
+              //   </Suspense>
+            }
+          />
+          {/* We should use Suspense whenever we want to implement React.lazy */}
+          <Route
+            path="/dashboard"
+            element={
+              //   <Suspense fallback={"loading..."}>
+              <Dashboard />
+              //   </Suspense>
+            }
+          />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+function Navbar() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>go to</div>
+      <button onClick={() => navigate("/")}>landing</button>
+      <button onClick={() => navigate("/dashboard")}>Dashboard</button>
+    </>
+  );
+}
